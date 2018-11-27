8515f257e68b2bd23c3782cddddf06e00c2e8e80
katsuNakajima <40666782+katsuNakajima@users.noreply.github.com>
2018-11-27 22:57:24 +0900

    add rtags key bind

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -248,14 +248,11 @@
 ;;;; rtags
 ;;;;--------------------------------------------------------
 (when (require 'rtags nil 'noerror)
-  (custom-set-variables '(rtags-use-helm t))
   (add-hook 'c-mode-common-hook
             (lambda ()
               (when (rtags-is-indexed)
-                (local-set-key (kbd "M-.") 'rtags-find-symbol-at-point)
-                (local-set-key (kbd "M-;") 'rtags-find-symbol)
-                (local-set-key (kbd "M-@") 'rtags-find-references)
-                (local-set-key (kbd "M-,") 'rtags-location-stack-back)))))
+                (local-set-key (kbd "M-,") 'rtags-find-symbol-at-point)
+                (local-set-key (kbd "M-;") 'rtags-find-symbol)))))
 
 ;;;;--------------------------------------------------------
 ;;;; helm
