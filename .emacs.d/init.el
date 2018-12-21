38506991a1eabd09307d56d5deb8504c26d6b156
katsuNakajima <40666782+katsuNakajima@users.noreply.github.com>
2018-12-21 15:18:08 +0900

    add rtags

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -247,6 +247,9 @@
 ;;;;--------------------------------------------------------
 ;;;; rtags
 ;;;;--------------------------------------------------------
+(add-hook 'c-mode-hook 'rtags-start-process-unless-running)
+(add-hook 'c++-mode-hook 'rtags-start-process-unless-running)
+
 (when (require 'rtags nil 'noerror)
   (add-hook 'c-mode-common-hook
             (lambda ()
