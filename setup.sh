05cea56bb87f6ca66c60f79b6c2284d2b757637c
katsuNakajima <40666782+katsuNakajima@users.noreply.github.com>
2019-06-14 22:32:27 +0900

    add vimrc

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DOTFILES_DIR=`pwd`
-DOT_FILES=( .clang-format .tmux.conf .zshrc .emacs.d )
+DOT_FILES=( .clang-format .tmux.conf .vimrc .zshrc .emacs.d )
 
 for file in ${DOT_FILES[@]}
 do
