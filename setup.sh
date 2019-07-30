e219f476a7b74977e5847c5b68e0bc9a7fbc091e
katsuNakajima <katugorou.karma@mineo.jp>
2019-07-31 00:43:12 +0900

    fix setup

--- setup.sh
+++ setup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 DOTFILES_DIR=`pwd`
-DOT_FILES=( .clang-format .tmux.conf .vimrc .zshrc .emacs.d )
+DOT_FILES=( .clang-format .tmux.conf .vimrc .zshrc .zshrc.Darwin .emacs.d )
 
 for file in ${DOT_FILES[@]}
 do
     if [ -d $DOTFILES_DIR/$file ]; then
-        ln -s $file $HOME
+        ln -s $DOTFILES_DIR/$file $HOME
     else
         ln -s $DOTFILES_DIR/$file $HOME/$file
     fi
