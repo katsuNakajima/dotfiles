ff24b71c84da98a2a35913ddf5ee14f7ff9251ec
katsuNakajima <40666782+katsuNakajima@users.noreply.github.com>
2020-03-16 11:15:44 +0900

    add zip

--- package.sh
+++ package.sh
@@ -1,1 +1,1 @@
-sudo apt install ibus-mozc zsh tilix emacs vim tmux xsel xclip tree curl build-essential cmake pkg-config git texinfo python3-ipython python3-venv python3-pip clang-format rapidjson-dev npm markdown pandoc -y
+sudo apt install ibus-mozc zsh tilix emacs vim tmux xsel xclip tree curl build-essential cmake pkg-config git texinfo python3-ipython python3-venv python3-pip clang-format rapidjson-dev npm markdown pandoc zip -y
