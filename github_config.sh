8c47f3eef1f2cc55c494bbb7f022085f5c5c8920
katsuNakajima <40666782+katsuNakajima@users.noreply.github.com>
2020-05-13 14:24:08 +0900

    add gitconfig

---
+++ github_config.sh
@@ -0,0 +1,2 @@
+git config --global user.name "KatsuNakajima"
+git config --global user.email "40666782+katsuNakajima@users.noreply.github.com"
---
+++ gitlab_config.sh
@@ -0,0 +1,2 @@
+git config --global user.name "KatsuNakajima"
+git config --global user.email "2562437-katsuNakajima@users.noreply.gitlab.com"
