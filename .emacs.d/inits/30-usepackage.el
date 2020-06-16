8d047101695171538c3f65589a44f0a3d578836f
KatsuNakajima <40666782+katsuNakajima@users.noreply.github.com>
2020-06-16 18:30:33 +0900

    add flymake atpoint

--- .emacs.d/inits/30-usepackage.el
+++ .emacs.d/inits/30-usepackage.el
@@ -207,13 +207,13 @@
   :config
   (setq flymake-no-changes-timeout 2))
 
-;; (use-package flymake-diagnostic-at-point
-;;   :after flymake
-;;   :custom
-;;   (flymake-diagnostic-at-point-timer-delay 0.1)
-;;   (flymake-diagnostic-at-point-display-diagnostic-function 'flymake-diagnostic-at-point-display-popup)
-;;   :hook
-;;   (flymake-mode . flymake-diagnostic-at-point-mode))
+(use-package flymake-diagnostic-at-point
+  :after flymake
+  :custom
+  (flymake-diagnostic-at-point-timer-delay 0.1)
+  (flymake-diagnostic-at-point-display-diagnostic-function 'flymake-diagnostic-at-point-display-popup)
+  :hook
+  (flymake-mode . flymake-diagnostic-at-point-mode))
 
 (use-package markdown-mode
   :mode (("\\.text\\'" . markdown-mode)
