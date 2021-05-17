3418fdccd54d5fe3e2a5e333113a71e3887eceac
KatsuNakajima <40666782+katsuNakajima@users.noreply.github.com>
2021-05-17 21:20:16 +0900

    add +x

--- fetch_rust-analyzer.sh
+++ fetch_rust-analyzer.sh
@@ -1,2 +1,2 @@
 #! /usr/local/bin/zsh
-curl -L https://github.com/rust-analyzer/rust-analyzer/releases/latest/download/rust-analyzer-mac -o ~/.cargo/bin/rust-analyzer
+curl -L https://github.com/rust-analyzer/rust-analyzer/releases/latest/download/rust-analyzer-mac -o ~/.cargo/bin/rust-analyzer && sudo chmod +x ~/.cargo/bin/rust-analyzer
