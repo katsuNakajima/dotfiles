b97867c7caf0deb3beb8c2b44ed6468dc234a8f4
KatsuNakajima <40666782+katsuNakajima@users.noreply.github.com>
2021-05-17 21:38:45 +0900

    fix rust-analyzer

--- .emacs.d/inits/30-usepackage.el
+++ .emacs.d/inits/30-usepackage.el
@@ -108,77 +108,13 @@
     )
 
 (use-package lsp-mode
-    :custom
-;; debug
-    (lsp-print-io nil)
-    (lsp-trace nil)
-    (lsp-print-performance nil)
-;; general
-    (lsp-auto-guess-root t)
-    (lsp-document-sync-method 'incremental) ;; always send incremental document
-    (lsp-response-timeout 5)
-    (lsp-prefer-flymake 'flymake)
-    (lsp-enable-completion-at-point nil)
-    (lsp-enable-on-type-formatting nil)
-    :bind
-    (:map lsp-mode-map
-        ("C-c r"   . lsp-rename))
-    :config
-    (require 'lsp-clients)
-;; LSP UI tools
-    (use-package lsp-ui
-    :custom
-    ;; lsp-ui-doc
-    (lsp-ui-doc-enable t)
-    (lsp-ui-doc-header t)
-    (lsp-ui-doc-include-signature t)
-    (lsp-ui-doc-position 'bottom) ;; top, bottom, or at-point
-    (lsp-ui-doc-max-width 150)
-    (lsp-ui-doc-max-height 30)
-    (lsp-ui-doc-use-childframe t)
-    (lsp-ui-doc-use-webkit nil)
-    ;; lsp-ui-flycheck
-    (lsp-ui-flycheck-enable nil)
-    ;; lsp-ui-sideline
-    (lsp-ui-sideline-enable nil)
-    (lsp-ui-sideline-ignore-duplicate t)
-    (lsp-ui-sideline-show-symbol t)
-    (lsp-ui-sideline-show-hover t)
-    (lsp-ui-sideline-show-diagnostics t)
-    (lsp-ui-sideline-show-code-actions t)
-    ;; lsp-ui-imenu
-    (lsp-ui-imenu-enable nil)
-    (lsp-ui-imenu-kind-position 'top)
-    ;; lsp-ui-peek
-    (lsp-ui-peek-enable t)
-    (lsp-ui-peek-peek-height 20)
-    (lsp-ui-peek-list-width 50)
-    (lsp-ui-peek-fontify 'on-demand) ;; never, on-demand, or always
-    :preface
-    (defun ladicle/toggle-lsp-ui-doc ()
-    (interactive)
-    (if lsp-ui-doc-mode
-        (progn
-        (lsp-ui-doc-mode -1)
-        (lsp-ui-doc--hide-frame))
-        (lsp-ui-doc-mode 1)))
-    :bind
-    (:map lsp-mode-map
-    ([remap xref-find-references] . lsp-ui-peek-find-references)
-    ([remap xref-find-definitions] . lsp-ui-peek-find-definitions)
-    ("C-c i"   . lsp-ui-peek-find-implementation)
-    ("C-c m"   . lsp-ui-imenu)
-    ("C-c s"   . lsp-ui-sideline-mode)
-    ("C-c f"   . lsp-format-buffer)
-    ("C-c d"   . ladicle/toggle-lsp-ui-doc))
-    :hook
-    (lsp-mode . lsp-ui-mode))
-;; Lsp completion
-    (use-package company-lsp
-    :custom
-    (company-lsp-cache-candidates t) ;; always using cache
-    (company-lsp-async t)
-    (company-lsp-enable-recompletion nil)))
+    :ensure t
+    :init (yas-global-mode)
+    :hook (rust-mode . lsp)
+    :bind ("C-c h" . lsp-describe-thing-at-point)
+    :custom (lsp-rust-server 'rust-analyzer))
+(use-package lsp-ui
+    :ensure t)
 
 ;; cclsは別途hookする
 (use-package ccls
@@ -311,19 +247,14 @@
     (editorconfig-mode 1)
 )
 
-(use-package cargo)
+(add-to-list 'exec-path (expand-file-name "~/.cargo/bin"))
+(use-package rust-mode
+    :ensure t
+    :custom rust-format-on-save t)
 
-(use-package rustic
-    :after
-    (lsp-mode cargo)
-    :bind
-    (:map
-        rustic-mode-map
-        ("C-c C-c C-r" . cargo-process-run))
-    :config
-    (setq rustic-lsp-server 'rust-analyzer)
-    (setq lsp-rust-analyzer-server-command '("~/.cargo/bin/rust-analyzer"))
-    )
+(use-package cargo
+    :ensure t
+    :hook (rust-mode . cargo-minor-mode))
 
 (use-package elpy
     :init
