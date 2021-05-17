153de210fcdc89315f32afe2c272a7d40fffd68f
KatsuNakajima <40666782+katsuNakajima@users.noreply.github.com>
2021-05-17 22:39:31 +0900

    company-lsp非推奨

--- .emacs.d/inits/30-usepackage.el
+++ .emacs.d/inits/30-usepackage.el
@@ -149,12 +149,6 @@
     (lsp-mode . lsp-ui-mode)
     )
 
-    (use-package company-lsp
-    :custom
-    (company-lsp-cache-candidates t) ;; always using cache
-    (company-lsp-async t)
-    (company-lsp-enable-recompletion nil))
-
 ;; cclsは別途hookする
 (use-package ccls
     :custom (ccls-executable "/usr/local/bin/ccls")
