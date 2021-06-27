a01d49d26c9e6e6ebf472878449f0e4fbcfdbed7
katsuNakajima <40666782+katsuNakajima@users.noreply.github.com>
2021-06-27 21:01:04 +0900

    update readme

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
 # Dotfiles
 
 ## install
+
 Execute `setup.sh` to make synbolic link
 
-### package install
-Execute `package.sh` to install packages
+## package install
+
+### For Mac
+
+Execute `brew bundle` to install Homebrew formulaes
+
+### For Debian or Ubuntu
+
+Execute `package.sh` to install apt packages
--- package.sh
+++ package.sh
@@ -1,1 +1,2 @@
-sudo apt install ibus-mozc zsh tilix emacs vim tmux xsel xclip tree curl build-essential cmake pkg-config git texinfo python3-ipython python3-venv python3-pip clang-format rapidjson-dev npm markdown pandoc zip peco -y
+#!/bin/bash
+sudo -E apt update && sudo -E apt install ibus-mozc zsh tilix emacs vim tmux xsel xclip tree curl build-essential cmake pkg-config git texinfo python3-ipython python3-venv python3-pip clang-format rapidjson-dev npm markdown pandoc zip peco -y
