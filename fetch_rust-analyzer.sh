5e8e83546937c5655dcf47ada140cde8a677961e
KatsuNakajima <40666782+katsuNakajima@users.noreply.github.com>
2022-03-19 17:22:51 +0900

    change fetch_rust-analyzer

--- fetch_rust-analyzer.sh
+++ fetch_rust-analyzer.sh
@@ -1,2 +1,2 @@
-#! /usr/local/bin/zsh
-curl -L https://github.com/rust-analyzer/rust-analyzer/releases/latest/download/rust-analyzer-mac -o ~/.cargo/bin/rust-analyzer && sudo chmod +x ~/.cargo/bin/rust-analyzer
+#! /bin/zsh
+curl -sL https://github.com/rust-analyzer/rust-analyzer/releases/latest/download/rust-analyzer-x86_64-unknown-linux-gnu.gz | zcat > ~/.cargo/bin/rust-analyzer && sudo chmod +x ~/.cargo/bin/rust-analyzer
