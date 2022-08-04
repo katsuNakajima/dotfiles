16a8c9e7df208ac9de482c4b65c73c2a3159098e
KatsuNakajima <40666782+katsuNakajima@users.noreply.github.com>
2022-08-04 09:46:54 +0900

    remove:rust-analyzer

--- fetch_rust-analyzer.sh
+++
@@ -1,2 +0,0 @@
-#! /bin/zsh
-curl -sL https://github.com/rust-analyzer/rust-analyzer/releases/latest/download/rust-analyzer-x86_64-unknown-linux-gnu.gz | zcat > ~/.cargo/bin/rust-analyzer && sudo chmod +x ~/.cargo/bin/rust-analyzer
