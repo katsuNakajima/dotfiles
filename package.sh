2da5faadf32674654ae3b13a3a9d5f46b56c074e
KatsuNakajima <40666782+katsuNakajima@users.noreply.github.com>
2022-09-02 22:13:55 +0900

    add:rust commond

--- package.sh
+++ package.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-sudo -E apt update && sudo -E apt install ibus-mozc zsh tilix vim tmux xsel xclip tree curl build-essential cmake pkg-config git texinfo python3-ipython python3-venv python3-pip clang-format rapidjson-dev npm markdown pandoc zip peco bat htop golang-go -y
+sudo -E apt update && sudo -E apt install ibus-mozc zsh tilix vim tmux xsel xclip tree curl build-essential cmake pkg-config git texinfo python3-ipython python3-venv python3-pip clang-format rapidjson-dev npm markdown pandoc zip peco htop golang-go -y
 
 # Rust and Rust-based-packages
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 cargo install cargo-edit cargo-make cargo-atcoder cargo-watch cross
 ## only linux
-cargo install exa
+cargo install exa bat fd-find ripgrep hexyl procs
 sh -c "$(curl -fsSL https://starship.rs/install.sh)"
 
 # Golang and Go-based-packages
